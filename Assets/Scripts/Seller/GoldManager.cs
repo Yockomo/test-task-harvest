7b39618875fb525909175bb3d3749f7ac9c6df5e
Yockomo <Funohs@yandex.ru>
2022-06-19 23:23:49 +0300

    Gold UI added

--- Assets/Scripts/Seller/GoldManager.cs
+++ Assets/Scripts/Seller/GoldManager.cs
@@ -6,14 +6,29 @@ public class GoldManager : MonoBehaviour
 {
     public float CurrentGold { get; private set; }
 
+    private UiManager uiManager;
+
+    public void Start()
+    {
+        try
+        {
+            uiManager = GetComponent<UiManager>();
+        }
+        catch
+        {
+            Debug.Log("There is no Ui Manager component");
+        }
+    }
+
     public void AddGold(float amount)
     {
         CurrentGold += amount;
-        Debug.Log(CurrentGold);
+        uiManager.UpdateGoldUi(CurrentGold.ToString());
     }
 
     public void TakeGold(float amount)
     {
         CurrentGold -= amount;
+        uiManager.UpdateGoldUi(CurrentGold.ToString());
     }
 }
